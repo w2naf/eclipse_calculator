30ba9fa464794cd3cbc7dd59fef081eded8a5906
Nathaniel A. Frissell <nathaniel.frissell@scranton.edu>
2022-05-07 06:45:17 -0400

    added new lat-lon map script

--- eclipse_maps_grid_squares.py
+++ eclipse_maps_grid_squares.py
@@ -15,21 +15,16 @@ import cartopy.crs as ccrs
 
 import eclipse_calc
 
-def location_dict(dlat,dlon,height,lat_0=-90.,lat_1=90,lon_0=-180.,lon_1=180):
-    """
-    Calculates a mesh grid of latitudes and longitudes at the center points of the
-    cells. Returns a flattened dataframe with these values plus a specified height.
-    """
-    dd              = {}
+def location_dict(precision=2,height=0.):
+    gs_grid     = eclipse_calc.locator.gridsquare_grid(precision=precision).flatten()
+    ll_grid     = eclipse_calc.locator.gridsquare2latlon(gs_grid)
+    lats,lons   = ll_grid
 
-    lats            = np.arange(lat_0+dlat/2.,lat_1+dlat/2.,dlat)
-    lons            = np.arange(lon_0+dlon/2.,lon_1+dlon/2.,dlon)
-
-    LATS, LONS      = np.meshgrid(lats,lons,indexing='ij')
-
-    dd['lat']       = LATS.flatten()
-    dd['lon']       = LONS.flatten()
-    dd['height']    = np.ones(dd['lat'].shape)*height
+    dd              = OrderedDict()
+    dd['grid']      = gs_grid
+    dd['lat']       = lats
+    dd['lon']       = lons
+    dd['height']    = np.ones(lats.shape)*height
     return dd
 
 def plot_eclipse_dict(run_dict):
@@ -48,41 +43,27 @@ def plot_eclipse(date,loc_dict,region='world',cmap=mpl.cm.gray_r,output_dir='out
 
     # Set up data dictionary.
     dd          = OrderedDict()
+    dd['grid']  = loc_dict['grid']
     dd['lat']   = loc_dict['lat']
     dd['lon']   = loc_dict['lon']
     dd['height']= loc_dict['height']
+#    locs        = loc_dict['loc']
 
     # Eclipse Magnitude
+#    dd['obsc']  = np.array([eclipse_calc.calculate_obscuration(date,loc=loc) for loc in locs])
     dates       = np.array(len(dd['lat'])*[date])
     dd['obsc']  = eclipse_calc.calculate_obscuration(dates,dd['lat'],dd['lon'],height=dd['height'])
+#    dd['obsc']  = dd['lat'] # Useful for debugging plotting.
 
     # Store into dataframe.
     df          = pd.DataFrame(dd)
+    df          = df.set_index('grid')
 
     # Save CSV Datafile.
-    csv_path    = fpath+'.csv.bz2'
+    csv_path    = fpath+'.csv'
     with open(csv_path,'w') as fl:
         fl.write('# Solar Eclipse Obscuration file for {!s}\n'.format(date))
     df.to_csv(csv_path,mode='a')
-
-
-    # Calculate vectors of center lats and lons.
-    center_lats = np.sort(df['lat'].unique())
-    center_lons = np.sort(df['lon'].unique())
-
-    # Find the lat/lon step size.
-    dlat = center_lats[1] - center_lats[0]
-    dlon = center_lons[1] - center_lons[0]
-
-    # Calculate vectors of boundary lats and lons.
-    lat_0   = center_lats.min() - dlat/2.
-    lat_1   = center_lats.max() + dlat/2.
-    lats    = np.arange(lat_0,lat_1+dlat,dlat)
-
-    lon_0 = center_lons.min() - dlon/2.
-    lon_1 = center_lons.max() + dlon/2.
-    lons    = np.arange(lon_0,lon_1+dlon,dlon)
-
 
     # Plot data.
     map_prm = {}
@@ -108,16 +89,8 @@ def plot_eclipse(date,loc_dict,region='world',cmap=mpl.cm.gray_r,output_dir='out
     ax          = fig.add_subplot(111,projection=ccrs.PlateCarree())
     hmap        = eclipse_calc.maps.HamMap(date,date,ax,show_title=False,**map_prm)
     hmap.overlay_gridsquares(label_precision=0,major_style={'color':'0.8','linestyle':'--'})
-
-    cshape      = (len(center_lats),len(center_lons))
-    obsc_arr    = dd['obsc'].reshape(cshape)
-    pcoll       = ax.pcolormesh(lons,lats,obsc_arr,vmin=vmin,vmax=vmax,cmap=cmap,zorder=5)
-
-    cbar_shrink = 0.5
-    cbar_label  = 'Obscuration'
-    cbar        = fig.colorbar(pcoll,label=cbar_label,shrink=cbar_shrink)
-    if cbar_ticks is not None:
-        cbar.set_ticks(cbar_ticks)
+    hmap.overlay_gridsquare_data(dd['grid'],dd['obsc'],vmin=vmin,vmax=vmax,cbar_ticks=cbar_ticks,
+                zorder=5,cmap=cmap,cbar_shrink=0.5,cbar_label='Obscuration')
 
     title       = '{!s} Height: {!s} km'.format(date.strftime('%d %b %Y %H%M UT'),height/1000.)
     fontdict    = {'size':'x-large','weight':'bold'}
@@ -126,31 +99,31 @@ def plot_eclipse(date,loc_dict,region='world',cmap=mpl.cm.gray_r,output_dir='out
     fig.savefig(fpath+'.png',bbox_inches='tight')
 
     plt.close(fig)
+
     return fpath
 
 if __name__ == '__main__':
     output_dir  = 'output'
     eclipse_calc.gen_lib.clear_dir(output_dir,php=True)
 
-#    # 21 August 2017 Total Solar Eclipse
-#    sDate   = datetime.datetime(2017,8,21,14)
-#    eDate   = datetime.datetime(2017,8,21,22)
+    # 21 August 2017 Total Solar Eclipse
+    sDate   = datetime.datetime(2017,8,21,14)
+    eDate   = datetime.datetime(2017,8,21,22)
 
 #    # 14 October 2023 Total Solar Eclipse
 #    sDate   = datetime.datetime(2023,10,14,14)
 #    eDate   = datetime.datetime(2023,10,14,21)
 
-    # 8 April 2024 Total Solar Eclipse
-    sDate   = datetime.datetime(2024,4,8,15)
-    eDate   = datetime.datetime(2024,4,8,21)
+#    # 8 April 2024 Total Solar Eclipse
+#    sDate   = datetime.datetime(2024,4,8,15)
+#    eDate   = datetime.datetime(2024,4,8,21)
 
     dt      = datetime.timedelta(minutes=5)
 
-    dlat        = 0.5
-    dlon        = 0.5
+    precision   = 4
     height      = 300e3
 
-    loc_dict    = location_dict(dlat,dlon,height)
+    loc_dict    = location_dict(precision=precision,height=height)
 
     run_list    = []
     cDate       = sDate
@@ -162,7 +135,7 @@ if __name__ == '__main__':
         run_list.append(tmp)
         cDate   += dt
 
-#    # Single Processor
+    # Single Processor
 #    for run_dict in run_list:
 #        fpath = plot_eclipse_dict(run_dict)
 
